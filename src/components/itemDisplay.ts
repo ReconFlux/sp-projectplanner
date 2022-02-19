20f800aad3995707feb28f8413e5c5417c467722
Stephen Burtrum <tsgtrecon117@gmail.com>
2022-02-19 10:19:25 -0600

    Added the item Display Component

---
+++ src/components/itemDisplay.ts
@@ -0,0 +1,18 @@
+import { ItemForm, Dashboard, DataTable, Navigation } from "dattatable";
+import { DataSource, IItem } from "../ds";
+import { Components } from "gd-sprest-bs";
+import * as jQuery from "jquery";
+import * as moment from "moment";
+
+/**
+ * Item Display
+ * Modal Popup when selecting an Item from the chart
+ */
+
+export class itemDisplay {
+    // VARS
+    // CONSTRUCTOR
+    // METHODS
+    // Render
+    private render(el: HTMLElement) { }
+}
